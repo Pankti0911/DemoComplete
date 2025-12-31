a2fa7c76a4123cd880babe8efa3f2de6df0d3ac0
Pankti0911 <pankti112004@gmail.com>
2025-12-31 16:24:15 +0530

    Update PatientCustomFieldValueRepository.java

--- SynerSence/src/main/java/com/synersence/hospital/repository/PatientCustomFieldValueRepository.java
+++ SynerSence/src/main/java/com/synersence/hospital/repository/PatientCustomFieldValueRepository.java
@@ -5,9 +5,10 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.synersence.hospital.entity.PatientCustomFieldValue;
+import com.synersence.hospital.entity.PatientMaster;
 
 public interface PatientCustomFieldValueRepository
-extends JpaRepository<PatientCustomFieldValue, Long> {
+        extends JpaRepository<PatientCustomFieldValue, Long> {
 
-List<PatientCustomFieldValue> findByPatientId(String patientId);
+    List<PatientCustomFieldValue> findByPatient(PatientMaster patient);
 }
